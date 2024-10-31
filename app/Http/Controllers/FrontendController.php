d6e0ee254c0a33f27a8940fc7cf57377b22549b6
Mahbub <mahbub.095@gmail.com>
2024-10-31 22:26:20 +0600

    Frontend logo & logout button fix

--- app/Http/Controllers/FrontendController.php
+++ app/Http/Controllers/FrontendController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Channel;
 use App\Models\HomePageSetting;
 use App\Models\Category;
+use App\Models\LogoSetting;
 use Illuminate\Http\Request;
 
 
@@ -19,11 +20,12 @@ class FrontendController extends Controller
     public function index()
     {
     
+        $logoSetting = LogoSetting::first();
         $categories=Category::select('name','id')->get();
         $channels=Channel::select('logo','name', 'slug','category_id')->latest()->get();
         $channelsData = Channel::paginate(10);
 
-        return view('frontend.home', compact('channels','categories','channelsData'));
+        return view('frontend.home', compact('channels','categories','channelsData','logoSetting'));
 
         // return response()->json(['category'=>$categories,'channel'=>$channels]);
 
--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -5,7 +5,7 @@
 
     <meta charset="utf-8">
 
-    <title>Eleven Plus Stream</title>
+    <title> @yield('title')</title>
     <!-- Latest compiled and minified CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 
@@ -45,7 +45,31 @@
 
 <body>
 
+    <div class="navbar navbar dark:#000">
+        <div class="container d-flex justify-content-between"><a href="/"><img src="{{asset($logoSetting->favicon)}}"
+                    width="130" height="75" alt="Jadoo Broadband"></a>
 
+            <form method="POST" action="{{ route('logout') }}">
+                @csrf
+
+
+                <button class="btn btn-outline-secondary btn-lg" type="submit"
+                    style="border-radius: 50%; width: 50px; height: 50px;">
+
+                    <span><svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 512 512"
+                            height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"
+                            style="margin: 2px -4px; font-size: 1.3em;">
+                            <path d="M192 277.4h189.7l-43.6 44.7L368 352l96-96-96-96-31 29.9 44.7 44.7H192v42.8z">
+                            </path>
+                            <path
+                                d="M255.7 421.3c-44.1 0-85.5-17.2-116.7-48.4-31.2-31.2-48.3-72.7-48.3-116.9 0-44.1 17.2-85.7 48.3-116.9 31.2-31.2 72.6-48.4 116.7-48.4 44 0 85.3 17.1 116.5 48.2l30.3-30.3c-8.5-8.4-17.8-16.2-27.7-23.2C339.7 61 298.6 48 255.7 48 141.2 48 48 141.3 48 256s93.2 208 207.7 208c42.9 0 84-13 119-37.5 10-7 19.2-14.7 27.7-23.2l-30.2-30.2c-31.1 31.1-72.5 48.2-116.5 48.2zM448.004 256.847l-.849-.848.849-.849.848.849z">
+                            </path>
+                        </svg></span>
+                    {{ __('') }}
+                </button>
+            </form>
+        </div>
+    </div>
 
     <div class="container-fluid">
         <div class="row justify-content-center" style="margin-top: 50px">
@@ -59,7 +83,6 @@
                     </div>
                 </div>
             </div>
-
 
             <div class="col-12 col-lg-4">
                 <div class="channel-list">
@@ -87,8 +110,7 @@
                                 </li>
                             @endif
                         @endforeach
-                     
-                     
+
                         @foreach(App\Models\Channel::all() as $channel)
 
                             <li class="{{ $channel->category_id }}"><a id="myLink" title="Click" href="javascript:;"
@@ -109,7 +131,17 @@
     <script src="{{asset('frontend/js/scripts.js')}}"></script>
     <script src="{{asset('frontend/js/Youtube.js')}}"></script>
     <script src="{{asset('frontend/js/Youtube.min.js')}}"></script>
-
+    <script>
+        @if ($errors->any())
+            @foreach ($errors->all() as $error)
+                toastr.error("{{$error}}")
+            @endforeach
+        @endif
+    </script>
+    <script>
+        $(document).ready(function () {
+            $('.auto_click').click();
+        })
+    </script>
 </body>
-
 </html>
