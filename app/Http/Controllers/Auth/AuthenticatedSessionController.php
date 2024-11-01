2f44707a56d067b4268bae1ebc46a7d9bac29dee
mahbub095 <mahbub.095@gmail.com>
2024-11-01 09:34:22 +0600

    Verification disable

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -39,8 +39,8 @@ class AuthenticatedSessionController extends Controller
 
         if($request->user()->role === 'admin'){
             return redirect()->intended('/admin/dashboard');
-        }elseif($request->user()->role === 'vendor'){
-            return redirect()->intended('/vendor/dashboard');
+        }elseif($request->user()->role === 'user'){
+            return redirect()->intended('/');
         }
 
 
